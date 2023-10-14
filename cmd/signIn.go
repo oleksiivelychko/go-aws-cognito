708664be79a7802c606042c5501060f225e8e09e
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-10-14 20:30:38 +0300

    create sign in command

---
+++ cmd/signIn.go
@@ -0,0 +1,64 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/oleksiivelychko/go-aws-cognito/service"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var signInCmd = &cobra.Command{
+	Use:   "sign-in",
+	Short: "Initiates sign-in for user in the Cognito user directory.",
+	Run: func(cmd *cobra.Command, args []string) {
+		srv, err := cognito.New(cfgAWS)
+		if err != nil {
+			cobra.CheckErr(err)
+		}
+
+		accessToken, err := srv.SignIn(
+			cmd.Flag("username").Value.String(),
+			cmd.Flag("password").Value.String(),
+			cmd.Flag("clientID").Value.String(),
+		)
+
+		if err != nil {
+			cobra.CheckErr(err)
+		}
+
+		var accessTokenTxt = "access-token.txt"
+
+		file, err := os.Create(accessTokenTxt)
+		if err != nil {
+			cobra.CheckErr(err)
+		}
+
+		defer func(file *os.File) {
+			_ = file.Close()
+		}(file)
+
+		_, err = file.Write([]byte(accessToken))
+		if err != nil {
+			cobra.CheckErr(err)
+		}
+
+		err = file.Sync()
+		if err != nil {
+			cobra.CheckErr(err)
+		}
+
+		fmt.Printf("✅ Access token has been written to %s\n", accessTokenTxt)
+	},
+}
+
+func init() {
+	signInCmd.Flags().StringP("username", "u", "", "")
+	signInCmd.Flags().StringP("password", "p", "", "")
+	signInCmd.Flags().String("clientID", "", "")
+
+	_ = signInCmd.MarkFlagRequired("username")
+	_ = signInCmd.MarkFlagRequired("password")
+	_ = signInCmd.MarkFlagRequired("clientID")
+
+	rootCmd.AddCommand(signInCmd)
+}
