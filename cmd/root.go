3798662181ffbe9f3d8a551b17d37d2c456578f3
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-10-14 10:45:16 +0300

    init root command

---
+++ cmd/root.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"github.com/oleksiivelychko/go-aws-cognito/config"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+)
+
+var cfgFile string
+var cfgAWS *config.AWS
+
+var rootCmd = &cobra.Command{
+	Use:   "go-aws-cognito",
+	Short: "Cognito user pools API allows set up user pools and app clients, and authenticate users.",
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "config.yaml", "config YAML file")
+}
+
+func initConfig() {
+	viper.SetConfigFile(cfgFile)
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		cobra.CheckErr(err)
+	}
+
+	cfgAWS = &config.AWS{
+		Region:             viper.Get("REGION").(string),
+		AwsAccessKeyId:     viper.Get("AWS_ACCESS_KEY_ID").(string),
+		AwsSecretAccessKey: viper.Get("AWS_SECRET_ACCESS_KEY").(string),
+		Endpoint:           viper.Get("ENDPOINT").(string),
+	}
+}
