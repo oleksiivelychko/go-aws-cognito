f8dfd9e80adc84b6a10ea3ec2a8de72899f8032b
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-10-14 12:46:38 +0300

    create pool command

---
+++ cmd/createPool.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/oleksiivelychko/go-aws-cognito/service"
+	"github.com/spf13/cobra"
+)
+
+var createPoolCmd = &cobra.Command{
+	Use:   "create-pool",
+	Short: "Creates a user pool.",
+	Run: func(cmd *cobra.Command, args []string) {
+		client, err := cognito.New(cfgAWS)
+		if err != nil {
+			cobra.CheckErr(err)
+		}
+
+		poolID, err := client.CreatePool(cmd.Flag("name").Value.String())
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf("✅ User pool ID: %s\n", poolID)
+		}
+	},
+}
+
+func init() {
+	createPoolCmd.Flags().String("name", "", "")
+
+	_ = createPoolCmd.MarkFlagRequired("name")
+
+	rootCmd.AddCommand(createPoolCmd)
+}
